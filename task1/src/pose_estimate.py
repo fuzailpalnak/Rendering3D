994d6c47476a71b4a7c263e10f0b58d60f696e68
fuzailpalnak <fuzailpalnak@gmail.com>
2022-11-04 11:32:55 +0100

    homography computation with webcam and video file

--- task1/src/pose_estimate.py
+++ task1/src/pose_estimate.py
@@ -1,38 +1,120 @@
+import os
+from typing import Union
+
 import cv2
 import numpy as np
 
-from task1.src.ops import create_point_map, ransac
+from task1.src.util import draw_inliers, draw_key_points
+from task1.src.ops import ransac, find_features, match_features
 
-MODEL_IMAGE = cv2.imread(r"/home/palnak/Workspace/Studium/msc/sem3/assignment/AR/task1/src/IMG_3435.jpg")
+MODEL_IMAGE = r"C:\Users\Fuzail.Palnak\UHD\openSource\AR\task1\data\IMG_3435.jpg"
+MP = r"../output/matches"
+KP = r"../output/keypoints"
+
+if not os.path.exists(KP):
+    os.makedirs(KP)
+
+if not os.path.exists(MP):
+    os.makedirs(MP)
+
+DEBUG = True
 
 
-def run():
-    cap = cv2.VideoCapture(0)
-    fps = cap.get(cv2.CAP_PROP_FPS)
+def run(pth: Union[str, int] = 0):
+    cap = cv2.VideoCapture(pth)
 
     # Check if the webcam is opened correctly
     if not cap.isOpened():
-        raise IOError("Cannot open webcam")
+        raise IOError("Cannot open THE provided")
 
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    writer = cv2.VideoWriter(
-        "pose_estimation.mp4", cv2.VideoWriter_fourcc(*"DIVX"), fps, (width, height)
-    )
+    # writer = cv2.VideoWriter(
+    #     "pose_estimation.mp4", cv2.VideoWriter_fourcc(*"DIVX"), cap.get(cv2.CAP_PROP_FPS), (width, height)
+    # )
 
-    loop = True
-    while loop:
-        print("ACCESS THROUGH VIDEO FEED")
-        _, img = cap.read()
+    i = 0
+    model_image = cv2.imread(MODEL_IMAGE)
 
-        point_map = create_point_map(MODEL_IMAGE, img)
+    while cap.isOpened():
+        print(f"\x1b[2K\r└──> Frame {i + 1}", end="")
+        _, frame = cap.read()
+
+        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        h, w = frame.shape[0:2]
+        model_image = cv2.resize(model_image, (w, h), interpolation=cv2.INTER_AREA)
+        model_image = cv2.rotate(model_image, cv2.ROTATE_90_CLOCKWISE)
+        model_image_key_points, model_image_desc = find_features(model_image)
+
+        # INTEREST POINT DETECTION
+        f_key_points, f_desc = find_features(frame)
+        if DEBUG:
+            cv2.imwrite(
+                os.path.join(KP, f"{i}_keypoints-1.png"),
+                cv2.drawKeypoints(model_image, model_image_key_points, model_image),
+            )
+            cv2.imwrite(
+                os.path.join(KP, f"{i}_keypoints-2.png"),
+                cv2.drawKeypoints(frame.copy(), f_key_points, frame.copy()),
+            )
+
+        # FEATURE MATCHING
+        matches = match_features(model_image_desc, f_desc)
+        point_map = np.array(
+            [
+                [
+                    model_image_key_points[match.queryIdx].pt[0],
+                    model_image_key_points[match.queryIdx].pt[1],
+                    f_key_points[match.trainIdx].pt[0],
+                    f_key_points[match.trainIdx].pt[1],
+                ]
+                for match in matches
+            ]
+        )
+
+        # HOMOGRAPHY ESTIMATION
         homography, inliers = ransac(point_map)
-        if homography is not None:
-            h, w = MODEL_IMAGE.shape
-            pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-            # project corners into frame
-            dst = cv2.perspectiveTransform(pts, homography)
-            # connect them with lines
-            frame = cv2.polylines(frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+        if DEBUG:
+            matches_sorted = sorted(matches, key=lambda x: x.distance)
+            src_pts = np.float32(
+                [model_image_key_points[m.queryIdx].pt for m in matches_sorted]
+            ).reshape(-1, 1, 2)
+            dst_pts = np.float32(
+                [f_key_points[m.trainIdx].pt for m in matches_sorted]
+            ).reshape(-1, 1, 2)
+            homography, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            print("└──>homography by OPENCV")
+            print(f"\n{homography}")
 
-run()
+            cv2.imwrite(
+                os.path.join(MP, f"{str(i)}.png"),
+                draw_inliers(frame.copy(), point_map, inliers),
+            )
+
+            cv2.imwrite(
+                os.path.join(KP, f"{i}_mapping.png"),
+                draw_key_points(model_image, frame.copy(), point_map, inliers=inliers),
+            )
+
+        # if homography is not None:
+        #     h, w = model_image.shape
+        #     pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(
+        #         -1, 1, 2
+        #     )
+        #     # project corners into frame
+        #     dst = cv2.perspectiveTransform(pts, homography)
+        #     # connect them with lines
+        #     frame = cv2.polylines(frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+        #     cv2.imwrite(
+        #         os.path.join(OUTPUT_PATH, f"{str(i)}_polylines.png"),
+        #         frame,
+        #     )
+        # cv2.imshow("img", img)
+        # cv2.imwrite("her.png", img)
+        # writer.write(img)
+
+        i += 1
+
+
+run(r"C:\Users\Fuzail.Palnak\UHD\openSource\AR\task1\data\IMG_3437_cropped.mp4")
---
+++ task1/src/util.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+
+BLUE = (255, 0, 0)
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+
+
+def draw_inliers(image, point_map, inliers):
+    """
+    inliers: set of (x1, y1) points
+    """
+    rows, cols = image.shape[0:2]
+    ret_image = np.zeros((rows, cols, 3), dtype="uint8")
+    ret_image[:, :, :] = image if image.ndim == 3 else np.dstack([image] * 3)
+
+    # Draw circles on top of the lines
+    for x1, y1, x2, y2 in point_map:
+        point = (int(x2), int(y2))
+        color = GREEN if (x1, y1, x2, y2) in inliers else RED
+        cv2.circle(ret_image, point, 4, color, 1)
+
+    return ret_image
+
+
+def draw_key_points(image1, image2, point_map, inliers=None, max_points=1000):
+    """
+    inliers: set of (x1, y1) points
+    """
+    rows1, cols1 = image1.shape[0:2]
+    rows2, cols2 = image2.shape[0:2]
+
+    match_image = np.zeros((max(rows1, rows2), cols1 + cols2, 3), dtype="uint8")
+    match_image[:rows1, :cols1, :] = (
+        image1 if image1.ndim == 3 else np.dstack([image1] * 3)
+    )
+    match_image[:rows2, cols1 : cols1 + cols2, :] = (
+        image2 if image2.ndim == 3 else np.dstack([image2] * 3)
+    )
+
+    small_point_map = [
+        point_map[i] for i in np.random.choice(len(point_map), max_points)
+    ]
+
+    # draw lines
+    for x1, y1, x2, y2 in small_point_map:
+        point1 = (int(x1), int(y1))
+        point2 = (int(x2 + image1.shape[1]), int(y2))
+        color = (
+            BLUE if inliers is None else (GREEN if (x1, y1, x2, y2) in inliers else RED)
+        )
+
+        cv2.line(match_image, point1, point2, color, 1)
+
+    # Draw circles on top of the lines
+    for x1, y1, x2, y2 in small_point_map:
+        point1 = (int(x1), int(y1))
+        point2 = (int(x2 + image1.shape[1]), int(y2))
+        cv2.circle(match_image, point1, 5, BLUE, 1)
+        cv2.circle(match_image, point2, 5, BLUE, 1)
+
+    return match_image
